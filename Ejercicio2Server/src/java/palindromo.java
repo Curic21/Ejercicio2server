f8de3facdfb02b0aa4c25e360a742d6c1fca9bba
Administrador <Administrador@192.168.1.11>
2013-10-06 20:17:49 +0200

    Ejercicio 2 jUnit

---
+++ Ejercicio2Server/src/java/palindromo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class palindromo {
+    
+    public String getPalindromo(String cadena){
+        return cadena;
+    }
+    public palindromo getReference() {
+        return this;
+    }
+    
+}
