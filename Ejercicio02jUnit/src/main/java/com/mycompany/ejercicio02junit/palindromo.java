fbf1c04362283d9777bfbb5248fd25c2c4f0474c
Administrador <Administrador@192.168.1.11>
2013-10-06 20:21:59 +0200

    Ejercicio 2 jUnit

---
+++ Ejercicio02jUnit/src/main/java/com/mycompany/ejercicio02junit/palindromo.java
@@ -0,0 +1,21 @@
+package com.mycompany.ejercicio02junit;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class palindromo {
+    
+    public String getPalindromo(String cadena){
+        return cadena;
+    }
+    public palindromo getReference() {
+        return this;
+    }
+    
+}
---
+++ Ejercicio02jUnit/src/test/java/palindromoTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.mycompany.ejercicio02junit.palindromo;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Administrador
+ */
+public class palindromoTest {
+
+    public palindromoTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @Test
+    public void testJunit() {
+        System.out.println("test: testJunit");
+        palindromo oTest = new palindromo();
+        assertNotNull("objeto palindromo creado", oTest);
+        assertEquals("Tiene que devolver la palabra seres", oTest.getPalindromo("seres"), "seres");
+        assertTrue("gePalindromo: Debe devolver seres", "seres".equals(oTest.getPalindromo("seres")));
+        if ("seres" != oTest.getPalindromo("seres")) {
+            fail("getPalindromo: Debe devolver seres ");
+        }      
+        assertSame("prueba getReference", oTest, oTest.getReference());
+        assertNotSame("no son el mismo objeto", oTest);
+    }
+}
